f65ec8548f2d3f43c285d0a2c9862fe911999945
AyushPatel-Hub <ayush292004patel@gmail.com>
2026-08-15 18:26:17 +0530

    first commited

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ export const Card = ({title,description,tags,photourl,link}) => {
     <div className=" h-130 w-180 hover:border p-6 flex flex-col items-center gap-4 justify-center hover:bg-white/10 rounded-3xl">
         <div className="flex flex-col items-center p-4 gap-5">
         
-          <Image src={photourl} height={200} width={400} alt="images" className="w-auto h-auto"/>
+          <Image src={photourl} height={200} width={400} alt="images" />
           
 
         <h1 className="text-3xl">{title}</h1>
